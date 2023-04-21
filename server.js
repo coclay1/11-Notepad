685c3eb9be53b627d51fa930260fc5002d184b35
coc lay <coc.lay97@gmail.com>
2023-04-20 21:16:55 -0400

    added saving to db

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const uuid = require('./helpers/uuid');
 
 const PORT = process.env.PORT || 3001;
 
@@ -12,7 +13,71 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html')))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html')))
+
+app.get('/api/notes', (req, res) => {
+    // Send a message to the client
+    res.status(200).json(`${req.method} request received to get notes`);
+
+    // Log our request to the terminal
+    console.info(`${req.method} request received to get notes`);
+});
+
+// POST request to add a review
+app.post('/api/notes', (req, res) => {
+    // Log that a POST request was received
+    console.info(`${req.method} request received to add a review`);
+  
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
+  
+    // If all the required properties are present
+    if (title && text) {
+      // Variable for the object we will save
+      const newReview = {
+        title,
+        text,
+        review_id: uuid(),
+      };
+  
+      // Obtain existing reviews
+      fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          // Convert string into JSON object
+          const parsedReviews = JSON.parse(data);
+  
+          // Add a new review
+          parsedReviews.push(newReview);
+  
+          // Write updated reviews back to the file
+          fs.writeFile(
+            './db/db.json',
+            JSON.stringify(parsedReviews, null, 4),
+            (writeErr) =>
+              writeErr
+                ? console.error(writeErr)
+                : console.info('Successfully updated reviews!')
+          );
+        }
+      });
+  
+      const response = {
+        status: 'success',
+        body: newReview,
+      };
+  
+      console.log(response);
+      res.status(201).json(response);
+    } else {
+      res.status(500).json('Error in posting review');
+    }
+  });
 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT} 🚀`)
