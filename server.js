757840eb669787cc3eba1d3b352f7fee478d8c5d
coc lay <coc.lay97@gmail.com>
2023-04-20 22:50:44 -0400

    connect remove

--- server.js
+++ server.js
@@ -14,70 +14,87 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/notes.html')))
+  res.sendFile(path.join(__dirname, '/public/notes.html')))
 
 app.get('/api/notes', (req, res) => {
-    res.status(200).json(allNotes);
-    // Log our request to the terminal
-    console.info(`${req.method} request received to get notes`);
+  res.status(200).json(allNotes);
+  // Log our request to the terminal
+  console.info(`${req.method} request received to get notes`);
 });
 
 // POST request to add a note
 app.post('/api/notes', (req, res) => {
-    // Log that a POST request was received
-    console.info(`${req.method} request received to add a notes`);
-  
-    // Destructuring assignment for the items in req.body
-    const { title, text } = req.body;
-  
-    // If all the required properties are present
-    if (title && text) {
-      // Variable for the object we will save
-      const newNote = {
-        title,
-        text,
-        id: uuid(),
-      };
-  
-      // Obtain existing notes
-      fs.readFile('./db/db.json', 'utf8', (err, data) => {
-        if (err) {
-          console.error(err);
-        } else {
-          // Convert string into JSON object
-          const parsedNote = JSON.parse(data);
-  
-          // Add a new note
-          parsedNote.push(newNote);
-  
-          // Write updated note back to the file
-          fs.writeFile(
-            './db/db.json',
-            JSON.stringify(parsedNote, null, 4),
-            (writeErr) =>
-              writeErr
-                ? console.error(writeErr)
-                : console.info('Successfully updated note!')
-          );
-        }
-      });
-  
-      const response = {
-        status: 'success',
-        body: newNote,
-      };
-  
-      console.log(response);
-      res.status(201).json(response);
+  // Log that a POST request was received
+  console.info(`${req.method} request received to add a notes`);
+
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+      id: uuid(),
+    };
+
+    // Obtain existing notes
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        // Convert string into JSON object
+        const parsedNote = JSON.parse(data);
+
+        // Add a new note
+        parsedNote.push(newNote);
+
+        // Write updated note back to the file
+        fs.writeFile(
+          './db/db.json',
+          JSON.stringify(parsedNote, null, 4),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info('Successfully updated note!')
+        );
+      }
+    });
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in posting note');
+  }
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+    if (err) {
+      console.error(err);
     } else {
-      res.status(500).json('Error in posting note');
+      const parsedNote = JSON.parse(data);
+      const deletedNote = parsedNote.filter(note => note.id !== req.params.id)
+      fs.writeFile(
+        './db/db.json',
+        JSON.stringify(deletedNote, null, 4),
+        (writeErr) => writeErr ? console.error(writeErr) : console.info('Successfully deleted a note')
+      )
     }
-  });
+  })
+})
+
 
 app.listen(PORT, () =>
-    console.log(`App listening at http://localhost:${PORT} 🚀`)
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
